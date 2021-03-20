ef127e6a66e47d0f2105562034aa87b6f31c4ff1
Jacopo Donati <33698919+jacopodonati@users.noreply.github.com>
2021-03-20 11:42:59 +0100

    Initial Commit

---
+++ Parcae/ParcaeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParcaeApp.swift
+//  Parcae
+//
+//  Created by Jacopo Donati on 20/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct ParcaeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
